91f3e185f3924e057abf9d1c252a13a6f5ffdb40
Glitch (hello-webpage) <none>
2021-04-21 10:55:53 +0000

    🐵💍 Checkpoint ./js/register.js:29751396/1966

--- js/register.js
+++ js/register.js
@@ -1,7 +1,17 @@
-const registerForm = document.querySelector('#register-form');
+const registerForm = document.querySelector("#register-form");
 
-registerForm.addEventListener('submit', (event) => {
+registerForm.addEventListener("submit", event => {
   event.preventDefault();
+
+  const firstName = document.querySelector("#first-name").value;
+  const lastName = document.querySelector("#last-name").value;
+  const mobileNumber = document.querySelector("#mobile-number").value;
+  const emailAddress = document.querySelector("#email-address").value;
+  const password = document.querySelector("#password").value;
+  const confirmPassword = document.querySelector("#confirm-password").value;
   
+  // Simple data validation here
+  const isValid = (firstName && lastName && mobileNumber.length === 11 && emailAddress && password && confirmPassword && password === confirmPassword);
   
+  console.log(isValid);
 });
