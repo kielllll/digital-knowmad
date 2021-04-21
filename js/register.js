25c8b2a88235a45f9bcb4919e647b8343a67f6ff
Glitch (hello-webpage) <none>
2021-04-21 12:03:18 +0000

    💿🐇 Checkpoint ./js/register.js:29751396/2867

--- js/register.js
+++ js/register.js
@@ -9,25 +9,32 @@ registerForm.addEventListener("submit", event => {
   const emailAddress = document.querySelector("#email-address").value;
   const password = document.querySelector("#password").value;
   const confirmPassword = document.querySelector("#confirm-password").value;
-  
+
   // Simple data validation here
-  const isValid = (firstName !== "" && lastName !== "" && mobileNumber.length === 13 && emailAddress !== "" && password !== "" && confirmPassword !== "" && password === confirmPassword);
-  
-  if(isValid) {
+  const isValid =
+    firstName !== "" &&
+    lastName !== "" &&
+    mobileNumber.length === 13 &&
+    emailAddress !== "" &&
+    password !== "" &&
+    confirmPassword !== "" &&
+    password === confirmPassword;
+
+  if (isValid) {
     fetch("https://zeke-csp2-app-server.herokuapp.com/api/users/register", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
       },
-      body: {
+      body: JSON.stringify({
         firstName,
         lastName,
         mobileNumber,
         emailAddress,
         password
-      }
+      })
     })
-    .then()
-    .then();
-  } else alert('Required field(s) is/are invalid.');
+      .then(response => response.json())
+      .then(data => console.dir(data));
+  } else alert("Required field(s) is/are invalid.");
 });
