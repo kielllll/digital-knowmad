785a323a356bf9bdda44b9893792a1bfba8a989e
Glitch (hello-webpage) <none>
2021-04-26 10:57:23 +0000

    🐯🌨 Checkpoint ./js/courses.js:29751396/298

--- js/courses.js
+++ js/courses.js
@@ -1,3 +1,12 @@
 const isAdmin = localStorage.getItem('isAdmin');
 const userId = localStorage.getItem('userId');
 
+const createCourse = document.querySelector('#admin-button');
+
+// Check if current user is an admin
+if(!isAdmin || isAdmin === "false") {
+ // not an admin
+  createCourse.innerHTML = null;
+} else {
+// an admin  
+}
