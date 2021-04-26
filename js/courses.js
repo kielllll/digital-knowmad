18712d635fd4d2c3f8d0580d7b22d585d6f2eea6
Glitch (hello-webpage) <none>
2021-04-26 12:34:16 +0000

    🌲🧀 Checkpoint ./js/courses.js:29751396/42 ./js/script.js:29751396/528 ./index.html:29751396/85

--- js/courses.js
+++ js/courses.js
@@ -1,6 +1,5 @@
 const createCourse = document.querySelector('#admin-button');
-console.log(isAdmin);
-console.log(userId);
+
 // Check if current user is an admin
 if(!isAdmin || isAdmin === "false") {
  // not an admin
--- js/script.js
+++ js/script.js
@@ -1,2 +1,12 @@
 const isAdmin = localStorage.getItem('isAdmin');
 const userId = localStorage.getItem('userId');
+
+// Select all navigation elements if user is logged-in
+const userLoggedInElements = document.querySelectorAll('.user-is-in');
+
+// Select all navigation elements if user is logged-out
+const userLoggedOutElements = document.querySelectorAll('.user-is-out');
+
+if(userId) {
+  
+}
