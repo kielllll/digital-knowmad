c25f247cb0634302485298179c7e7c09fb6bf84f
Glitch (hello-webpage) <none>
2021-04-27 12:21:21 +0000

    🏰🏝 Checkpoint ./js/profile.js:29751396/1173

--- js/profile.js
+++ js/profile.js
@@ -1,6 +1,18 @@
-const profileContainer = document.querySelector('#profile-container');
+const profileContainer = document.querySelector("#profile-container");
 
 // Check if a user is logged-in
 if (userId && typeof userId === "string") {
-  alert("You are currently logged in.");
+  fetch(
+    `https://zeke-csp2-app-server.herokuapp.com/api/users/details?id=${userId}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+  )
+    .then(res => res.json())
+    .then(data => {
+      let {firstName, lastName, email} = data.userDetails;
+  });
 } else window.location.replace("./login.html");
