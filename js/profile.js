8fddf7301e4e80642007c21a97ed8ed9a0877f12
Glitch (hello-webpage) <none>
2021-04-27 12:09:41 +0000

    🌥🚅 Checkpoint ./js/profile.js:29751396/1116

--- js/profile.js
+++ js/profile.js
@@ -1,1 +1,6 @@
 const profileContainer = document.querySelector('#profile-container');
+
+// Check if a user is logged-in
+if (userId && typeof userId === "string") {
+  alert("You are currently logged in.");
+} else window.location.replace("./login.html");
