3d5a3bfebe31f3c4cf3cca95561f68c92c793d89
Glitch (hello-webpage) <none>
2021-04-27 13:07:44 +0000

    🎍🏪 Checkpoint ./js/new_course.js:29751396/106 ./js/courses.js:29751396/99

--- js/courses.js
+++ js/courses.js
@@ -1,7 +1,7 @@
 const createCourse = document.querySelector('#admin-button');
 
 // Check if current user is an admin
-if(!isAdmin || isAdmin === "false") {
+if(!isAdmin) {
  // not an admin
   createCourse.innerHTML = null;
 }
--- js/new_course.js
+++ js/new_course.js
@@ -1,4 +1,9 @@
 // Check if a user is logged-in
 if (userId && typeof userId === "string") {
- 
+  // Check if user is an admin
+  if(isAdmin && isAdmin === true) {
+    
+  } else {
+    
+  }
 } else window.location.replace("./login.html");
