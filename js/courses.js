4d52001207307c424e69a0aef8af9d0c94a0f8a6
Glitch (hello-webpage) <none>
2021-04-28 12:26:37 +0000

    🏞🎾 Checkpoint ./courses.html:29751396/3 ./js/courses.js:29751396/826

--- js/courses.js
+++ js/courses.js
@@ -1,7 +1,14 @@
-const createCourse = document.querySelector('#admin-button');
+const createCourse = document.querySelector("#admin-button");
 
 // Check if current user is an admin
-if(!isAdmin) {
- // not an admin
+if (!isAdmin) {
+  // not an admin
   createCourse.innerHTML = null;
 }
+
+// Diplay all courses
+fetch(`https://zeke-csp2-app-server.herokuapp.com/api/courses`)
+  .then(res => res.json())
+  .then(data => {
+    console.dir(data);
+  });
