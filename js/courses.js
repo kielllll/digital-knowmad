54a7e3b2c25e638329872a0bc88408ec3df5be67
Glitch (hello-webpage) <none>
2021-04-28 12:37:59 +0000

    🌜💶 Checkpoint ./js/courses.js:29751396/6659

--- js/courses.js
+++ js/courses.js
@@ -10,5 +10,36 @@ if (!isAdmin) {
 fetch(`https://zeke-csp2-app-server.herokuapp.com/api/courses`)
   .then(res => res.json())
   .then(data => {
-    console.dir(data);
+    // Display all courses
+    const course = data.courses.map(course => `
+      <div class="col-md-6 my-3">
+                 <div class='card'>
+              <div class='card-body'>
+                  <h5 class='card-title'>${course.name}</h5>
+                  <p class='card-text text-left'>
+                      ${course.description}
+                  </p>
+                  <p class='card-text text-right'>
+                     ₱ ${course.price}
+                  </p>
+              </div>
+              <div class='card-footer'>
+                  <div class="row">
+                    <div class="col-sm-12 col-md-4 mb-1">
+                      <a href="./course.html?id=${course._id}" class="btn btn-outline-primary btn-block btn-course">View</a>
+                    </div>
+                    <div class="col-sm-12 col-md-4 mb-1">
+                      <a href="./edit_course.html?id=${course._id}" class="btn btn-outline-secondary btn-block btn-course">Edit</a>
+                    </div>
+                    <div class="col-sm-12 col-md-4 mb-1">
+                      <a href="./delete_course.html?id=${course._id}" class="btn btn-outline-danger btn-block btn-course">Delete</a>
+                    </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+    `);
+    document.querySelector(
+      "#courses-container"
+    ).innerHTML = course.join("");
   });
