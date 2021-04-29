e0457466aee9e4fa9aabe549337e6bfdcda6a396
Glitch (hello-webpage) <none>
2021-04-29 11:49:44 +0000

    🐩🚇 Checkpoint ./js/course.js:29751396/916

--- js/course.js
+++ js/course.js
@@ -1,3 +1,8 @@
 // Convert the window.location.search to a query string object
 const queryStringObject = new URLSearchParams(window.location.search);
 const courseId = queryStringObject.get("id");
+
+// Send a request to retrieve a course description
+fetch(`https://zeke-csp2-app-server.herokuapp.com/api/courses?id=${courseId}`)
+  .then()
+  .then();
